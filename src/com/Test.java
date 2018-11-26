1c6b5715fd5df144c99d8f8075367c4035e7aa4c
haha <1453343346@qq.com>
2018-11-26 20:21:34 +0800

    222

---
+++ src/com/Test.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("哈哈");
+        System.out.println("哈哈");
+        System.out.println("哈哈");
+        System.out.println("哈哈");
+    }
+
+ public void  l(){
+        int a=10;
+        int b=a+10;
+ }
+
+}
